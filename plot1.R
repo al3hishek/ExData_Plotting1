9b6a23c526375b5c05c3a9d5dda67455332f2df6
al3hishek <abhishekshettynov19@gmail.com>
2015-01-23 15:48:42 +0530

    Ex-DataPlotting2

---
+++ plot1.R
@@ -0,0 +1,13 @@
+setwd("C:\\a\\Statistical Inference")
+## This first line will likely take a few seconds. Be patient!
+NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+View(NEI)
+totalPM25ByYear <- tapply(NEI$Emissions, NEI$year, sum)
+totalPM25ByYear
+
+png("plot1.png")
+plot(names(totalPM25ByYear), totalPM25ByYear, type="l",
+     xlab="Year", ylab=expression("Total" ~ PM[2.5] ~ "Emissions (tons)"),
+     main=expression("Total US" ~ PM[2.5] ~ "Emissions by Year"))
+dev.off()
